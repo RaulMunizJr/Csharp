39d331a39f74ed5eb04b6fffdcedefc519b176c0
Raul Muniz <Raul.Muniz01@utrgv.edu>
2019-06-22 18:32:38 -0500

    Create Tuple.cs

---
+++ Basics/Tuple.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningTuple //List of different types
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Tuple<int, string, bool> myTuple = new Tuple<int, string, bool>(1, "hello", true);
+
+			var myTuple = Tuple.Create(1, "hello", true); //can be created either way.
+
+			//Console.WriteLine(myTuple.Item1);
+			//Console.ReadLine();
+		}
+	}
+}
