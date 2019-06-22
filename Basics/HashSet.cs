f19f673753a974da8d814f8a74d135a7bae8d4bb
Raul Muniz <Raul.Muniz01@utrgv.edu>
2019-06-22 18:53:00 -0500

    Q and H

---
+++ Basics/HashSet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningHashSet
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var myHash = new HashSet<string>(); //removes duplicates!
+
+			myHash.Add("hello");
+			myHash.Add("hello");
+
+			string[] s = new string[] { "hello" };
+
+			Console.WriteLine(myHash.Count);
+
+			//A quick way to make comparison if value is contained in the other structure
+			Console.WriteLine(myHash.Overlaps(s));
+
+			//Console.ReadLine();
+		}
+	}
+}
---
+++ Basics/Queue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningQueue //First in, First out
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Queue<int> myQueue = new Queue<int>();
+
+			myQueue.Enqueue(1);
+			myQueue.Enqueue(2);
+			myQueue.Enqueue(3);
+
+			foreach (var item in myQueue)
+			{
+				Console.WriteLine(item);
+			}
+
+			Console.WriteLine(myQueue.Dequeue()); //displays and removes first item
+			Console.WriteLine(myQueue.Peek()); //displays but doesnt remove
+
+			//Console.ReadLine();
+		}
+	}
+}
